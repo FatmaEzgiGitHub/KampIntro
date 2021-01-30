f691d6d2aad10eda4afd8fca8b4766078883af22
Fatma Ezgi Yıldırım <ftmzgyldrm2012@gmail.com>
2021-01-30 17:10:52 +0300

    17 Ocak 2021

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();//BİR CLASS'I BUŞEKİLDE OLUŞTURUP VE REFERANS ALIYORDUK.
+            isimler.Add("Engin");
+
+
+
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
